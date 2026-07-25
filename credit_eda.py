a07141072f29edc14fa079a1c663a064447763f7
Bushra Shaikh <129250775+BushraShaikh7@users.noreply.github.com>
2026-07-25 11:48:29 +0100

    Implement logistic regression and random forest models
    Added logistic regression and random forest models for credit risk prediction.

--- credit_eda.py
+++ credit_eda.py
@@ -150,3 +150,67 @@ plt.xlabel("NumberOfTimes90DaysLate")
 plt.ylabel("NumberOfPeople")
 plt.savefig("latepayment_vs_default.png")
 plt.close()
+
+######################################################
+## NEW CODE ## LOGISTIC REGRESSION
+######################################################
+
+from sklearn.model_selection import train_test_split
+print("train_test_split loaded")
+from sklearn.linear_model import LogisticRegression
+print("logisticRegression loaded")
+
+##STEP:2##
+X=dataframe.drop(columns=["Unnamed: 0","SeriousDlqin2yrs"]) 
+y=dataframe['SeriousDlqin2yrs']
+
+##STEP:3##
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+print(X_train)
+print(y_test)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+##STEP:4##
+model=LogisticRegression(class_weight="balanced", max_iter=1000)
+
+##STEP5##
+model.fit(X_train_scaled, y_train)
+y_pred = model.predict(X_test_scaled)
+y_pred_proba = model.predict_proba(X_test_scaled)
+
+print("Model trained successfully")
+
+from sklearn.metrics import classification_report, roc_auc_score
+
+print(classification_report(y_test, y_pred))
+print("ROC-AUC Score:", roc_auc_score(y_test, y_pred_proba[:, 1]))
+
+######################################################
+## NEW CODE ## RANDOM FOREST
+######################################################
+
+from sklearn.ensemble import RandomForestClassifier
+print("random forest imported")
+from sklearn.model_selection import train_test_split
+print("train_test_split imported")
+
+## Step 3 - 
+rf_model=RandomForestClassifier(class_weight="balanced", n_estimators=100, random_state=42)
+rf_model.fit(X_train,y_train)
+rf_pred=rf_model.predict(X_test)
+rf_pred_proba=rf_model.predict_proba(X_test)
+
+print("model radom forest ran succesfully")
+
+from sklearn.metrics import classification_report, roc_auc_score
+
+print(classification_report(y_test, rf_pred))
+print("ROC-AUC Score:", roc_auc_score(y_test, rf_pred_proba[:, 1]))
+
+rf_train_pred = rf_model.predict(X_train)
+print("TRAINING PERFORMANCE:")
+print(classification_report(y_train, rf_train_pred))
